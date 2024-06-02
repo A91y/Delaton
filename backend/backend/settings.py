d511e754ba6dfde2628ba6c655f7c242ecd5e7d1
A91y <ayush.agr254@gmail.com>
2024-06-02 15:39:19 +0530

    fix: no secret key fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    SECRET_KEY = '3848e9eff8d31953e3762251e377bb08f78099d2f2193740cd0523738da8b1a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
